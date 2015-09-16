bd1faa182462bb0bcb8a19286496fc2f1aa8c07c
ridwanarvi <ridwanarvi@gmail.com>
2015-09-16 14:51:14 +0700

    Tutorial 2 done

--- lists/tests.py
+++ lists/tests.py
@@ -30,23 +30,14 @@ class HomePageTest(TestCase):
        request = HttpRequest()
        response = home_page(request)
        count = Item.objects.count()
-       comment = self.browser.find_element_by_id('komentar')
        
        if count == 0:
-           self.assertEqual(
-                   comment.text,
-                   'yey, waktunya berlibur'
-           )
+           self.assertIn('yey, waktunya berlibur', response.content.decode())
        elif count < 5:
-           self.assertEqual(
-                   comment.text,
-                   'sibuk tapi santai'
-           )
+           self.assertIn('sibuk tapi santai', response.content.decode())
        else :
-           self.assertEqual(
-                   comment.text,
-                   'oh tidak'
-           )
+           self.assertIn('oh tidak', response.content.decode())
+           
    def test_home_page_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
@@ -56,16 +47,6 @@ class HomePageTest(TestCase):
 
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
-   def test_home_page_can_save_a_POST_request(self):
-       request = HttpRequest()
-       request.method = 'POST'
-       request.POST['item_text'] = 'A new list item'
-
-       response = home_page(request)
-
-       self.assertEqual(Item.objects.count(), 1)
-       new_item = Item.objects.first()
-       self.assertEqual(new_item.text, 'A new list item')
 
 
    def test_home_page_redirects_after_POST(self):
@@ -91,11 +72,11 @@ class HomePageTest(TestCase):
    def test_home_page_returns_correct_html(self):
       request = HttpRequest()
       response = home_page(request)
-      expected_html = render_to_string('home.html')
-      self.assertEqual(response.content.decode(), expected_html)
-      #self.assertTrue(response.content.startswith(b'<html>'))
-      #self.assertIn(b'<title>To-Do lists</title>', response.content)
-      #self.assertTrue(response.content.strip().endswith(b'</html>'))
+      #expected_html = render_to_string('home.html')
+      #self.assertEqual(response.content.decode(), expected_html)
+      self.assertTrue(response.content.startswith(b'<html>'))
+      self.assertIn(b'<title>To-Do lists</title>', response.content)
+      self.assertTrue(response.content.strip().endswith(b'</html>'))
       
    def test_home_page_can_save_a_POST_request(self):
        request = HttpRequest()
@@ -104,13 +85,9 @@ class HomePageTest(TestCase):
 
        response = home_page(request)
 
-       self.assertEqual(Item.objects.count(), 1)  #1
-       new_item = Item.objects.first()  #2
-       self.assertEqual(new_item.text, 'A new list item')  #3
+       self.assertEqual(Item.objects.count(), 1)
+       new_item = Item.objects.first()
+       self.assertEqual(new_item.text, 'A new list item')
 
-       self.assertIn('A new list item', response.content.decode())
-       expected_html = render_to_string(
-           'home.html',
-           {'new_item_text':  'A new list item'}
-       )
-       self.assertEqual(response.content.decode(), expected_html)
+       self.assertEqual(response.status_code, 302)
+       self.assertEqual(response['location'], '/')
