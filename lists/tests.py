fae4314a1e7061299f6cef1b1abba23c7c32dddc
ridwanarvi <ridwanarvi@gmail.com>
2015-09-16 10:33:42 +0700

    add some untracked file

--- lists/tests.py
+++ lists/tests.py
@@ -26,6 +26,33 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 class HomePageTest(TestCase):
 
+   def test_home_page_can_save_a_POST_request(self):
+       request = HttpRequest()
+       request.method = 'POST'
+       request.POST['item_text'] = 'A new list item'
+
+       response = home_page(request)
+
+       self.assertEqual(Item.objects.count(), 1)
+       new_item = Item.objects.first()
+       self.assertEqual(new_item.text, 'A new list item')
+
+
+   def test_home_page_redirects_after_POST(self):
+       request = HttpRequest()
+       request.method = 'POST'
+       request.POST['item_text'] = 'A new list item'
+
+       response = home_page(request)
+
+       self.assertEqual(response.status_code, 302)
+       self.assertEqual(response['location'], '/')
+
+   def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+        
    def test_root_url_resolves_to_home_page_view(self):
       found = resolve('/')
       self.assertEqual(found.func, home_page)
@@ -41,15 +68,19 @@ class HomePageTest(TestCase):
       #self.assertTrue(response.content.strip().endswith(b'</html>'))
       
    def test_home_page_can_save_a_POST_request(self):
-      request = HttpRequest()
-      request.method = 'POST'
-      request.POST['item_text'] = 'A new list item'
+       request = HttpRequest()
+       request.method = 'POST'
+       request.POST['item_text'] = 'A new list item'
 
-      response = home_page(request)
+       response = home_page(request)
 
-      self.assertIn('A new list item', response.content.decode())
-      expected_html = render_to_string(
-          'home.html',
-          {'new_item_text':  'A new list item'}
-      )
-      self.assertEqual(response.content.decode(), expected_html)
+       self.assertEqual(Item.objects.count(), 1)  #1
+       new_item = Item.objects.first()  #2
+       self.assertEqual(new_item.text, 'A new list item')  #3
+
+       self.assertIn('A new list item', response.content.decode())
+       expected_html = render_to_string(
+           'home.html',
+           {'new_item_text':  'A new list item'}
+       )
+       self.assertEqual(response.content.decode(), expected_html)
