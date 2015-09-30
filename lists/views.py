5ba16abf7dcf3930af6b6b599fae63edb196e6ed
ridwanarvi <ridwanarvi@gmail.com>
2015-09-30 13:45:06 +0700

    Another URL and View for Adding List Items

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,6 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
     
@@ -16,3 +13,6 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
