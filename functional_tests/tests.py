7ad75d11448f90db1e4cea124d789ddc42d76a50
ridwanarvi <ridwanarvi@gmail.com>
2015-09-30 10:14:14 +0700

    Implementing the New Design Using TDD

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,7 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
        # She types "Buy peacock feathers" into a text box (Edith's hobby
        # is tying fly-fishing lures)
        inputbox.send_keys('Buy peacock feathers')
-
+       edith_list_url = self.browser.current_url
+       self.assertRegex(edith_list_url, '/lists/.+') #1
        # When she hits enter, the page updates, and now the page lists
        # "1: Buy peacock feathers" as an item in a to-do list table
        inputbox.send_keys(Keys.ENTER)
@@ -124,7 +125,35 @@ class NewVisitorTest(LiveServerTestCase):
                'oh tidak'
        )
        
+       ## We use a new browser session to make sure that no information
+       ## of Edith's is coming through from cookies etc #1
+       self.browser.quit()
+       self.browser = webdriver.Firefox()
        
+       # Francis visits the home page.  There is no sign of Edith's
+       # list
+       self.browser.get(self.live_server_url)
+       page_text = self.browser.find_element_by_tag_name('body').text
+       self.assertNotIn('Buy peacock feathers', page_text)
+       self.assertNotIn('make a fly', page_text)
+
+       # Francis starts a new list by entering a new item. He
+       # is less interesting than Edith...
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       inputbox.send_keys('Buy milk')
+       inputbox.send_keys(Keys.ENTER)
+
+       # Francis gets his own unique URL
+       francis_list_url = self.browser.current_url
+       self.assertRegex(francis_list_url, '/lists/.+')
+       self.assertNotEqual(francis_list_url, edith_list_url)
+
+       # Again, there is no trace of Edith's list
+       page_text = self.browser.find_element_by_tag_name('body').text
+       self.assertNotIn('Buy peacock feathers', page_text)
+       self.assertIn('Buy milk', page_text)
+
+       # Satisfied, they both go back to sleep
        
        # Edith wonders whether the site will remember her list. Then she sees
        # that the site has generated a unique URL for her -- there is some
