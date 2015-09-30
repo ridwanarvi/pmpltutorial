212e05ddbfffb38605b2e9de12c1737731f4757d
ridwanarvi <ridwanarvi@gmail.com>
2015-09-30 10:24:17 +0700

    new url,view and template to display lists (error)

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,21 @@ from lists.views import home_page
 from lists.models import Item
 from django.template.loader import render_to_string
 
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/') #1
+
+        self.assertContains(response, 'itemey 1') #2
+        self.assertContains(response, 'itemey 2') #3
+        
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -54,16 +69,6 @@ class HomePageTest(TestCase):
        response = home_page(request)
        self.assertIn('oh tidak', response.content.decode())
 
-       
-   def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
    def test_home_page_redirects_after_POST(self):
@@ -74,7 +79,7 @@ class HomePageTest(TestCase):
        response = home_page(request)
 
        self.assertEqual(response.status_code, 302)
-       self.assertEqual(response['location'], '/')
+       self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
    def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
