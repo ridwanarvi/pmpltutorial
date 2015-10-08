0e830b001c1ba8913d63457236d481ccd68cde58
ridwanarvi <ridwanarvi@gmail.com>
2015-10-08 11:02:43 +0700

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,6 +11,7 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
+        
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -101,5 +102,18 @@ class NewVisitorTest(LiveServerTestCase):
       # She visits that URL - her to-do list is still there.
 
       # Satisfied, she goes back to sleep
+    
+    def test_layout_and_styling(self):
+            # Edith goes to the home page
+            self.browser.get(self.live_server_url)
+            self.browser.set_window_size(1024, 768)
+
+            # She notices the input box is nicely centered
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+            )
 if __name__ == '__main__':
    unittest.main(warnings='ignore')
