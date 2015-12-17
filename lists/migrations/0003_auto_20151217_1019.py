16516d2d8cdfb708fa0045352781334a32c4a2e9
Ridwan Arvihafiz Bakri <ridwanarvi@gmail.com>
2015-12-17 17:31:41 +0700

    change migrations

--- lists/migrations/0003_auto_20151217_1019.py
+++ lists/migrations/0003_auto_20151217_1019.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('lists', '0005_list'),
+        ('lists', '0002_item_text'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+            ],
+        ),
         migrations.AddField(
             model_name='item',
             name='list',
